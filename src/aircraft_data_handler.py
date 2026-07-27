19df7d1bb7909c6d64fb3918b95517f861f524ed
Your Name <you@example.com>
2026-07-27 19:33:34 +0300

    Реализация методов сравнения class Aeroplane

--- src/aircraft_data_handler.py
+++ src/aircraft_data_handler.py
@@ -10,12 +10,34 @@ class Aeroplane:
 
     @classmethod
     def cast_to_object_list(cls, country_aeroplanes: list[list]):
-        aeroplane_dict = []
+        aeroplane_list = []
         for aeroplane in country_aeroplanes:
             callsign = aeroplane[1]
             country = aeroplane[2]
             velocity = aeroplane[9]
             altitude = aeroplane[7]
             single_aeroplane = Aeroplane(callsign, country, velocity, altitude)
-            aeroplane_dict.append(single_aeroplane)
-        return aeroplane_dict
+            aeroplane_list.append(single_aeroplane)
+        return aeroplane_list
+
+    def __eq__(self, other):
+        """Равенство объектов (сравнение самолетов между собой по скорости и высоте)"""
+        if self.velocity == other.velocity and self.altitude == other.altitude:
+            return True
+        return False
+
+    def __gt__(self, other):
+        """Больше (сравнение самолетов между собой по скорости и высоте)"""
+        if self.velocity > other.velocity:
+            return True
+        if self.altitude > other.altitude:
+            return True
+        return False
+
+    def __lt__(self, other):
+        """Меньше (сравнение самолетов между собой по скорости и высоте)"""
+        if self.velocity < other.velocity:
+            return True
+        if self.altitude < other.altitude:
+            return True
+        return False
