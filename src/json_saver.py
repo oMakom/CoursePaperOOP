39b9c88ddb46d1a20c3ad10c35dd7a5572df2d18
Your Name <you@example.com>
2026-07-29 21:02:47 +0300

    Реализация SONSaver(BaseSaver) и асбтрактного класса

--- src/json_saver.py
+++ src/json_saver.py
@@ -1,12 +1,22 @@
 import json
 import os
+from abc import ABC, abstractmethod
 
 from src.aircraft_data_handler import Aeroplane
 
 root_path = os.path.dirname(os.path.abspath(__file__))
 data_path = os.path.abspath(os.path.join(root_path, "..", "data/data_aeroplanes.json"))
 
-class JSONSaver:
+class BaseSaver(ABC):
+    @abstractmethod
+    def add_aeroplane(self, aeroplane: "list[Aeroplane] | Aeroplane") -> None:
+        pass
+
+    @abstractmethod
+    def delete_aeroplane(self, criteria: str) -> None:
+        pass
+
+class JSONSaver(BaseSaver):
     """ класс для добавления и удаления информации о самолетах в/из JSON-файл """
     def __init__(self):
         self.data_path = data_path
@@ -36,6 +46,11 @@ class JSONSaver:
             data_f = []
         return data_f
 
+    def write_to_json(self, data_list: list):
+        """ Записывает данные в файл "../data/data_aeroplanes.json" """
+        with open(self.data_path, "w", encoding="utf-8") as f:
+            json.dump(data_list, f, ensure_ascii=False, indent=4)
+
     def add_aeroplane(self, aeroplane: "list[Aeroplane] | Aeroplane"):
         """ класс для сохранения информации о самолетах в JSON-файл """
         # Чтение переданных данных
@@ -54,8 +69,7 @@ class JSONSaver:
                     key.add(item["callsign"])
                     data_file.append(item)
             # Перезаписываем файл без дублей
-            with open(self.data_path, "w", encoding="utf-8") as f:
-                json.dump(data_file, f, ensure_ascii=False, indent=4)
+            JSONSaver.write_to_json(self, data_file)
         else:
             # Файла нет - создаем новый
             date_new = []
@@ -66,8 +80,15 @@ class JSONSaver:
                 if item.get("callsign") is not None and item.get("callsign") not in key:
                     key.add(item["callsign"])
                     date_new.append(item)
-            with open(self.data_path, "w", encoding="utf-8") as f:
-                json.dump(date_new, f, ensure_ascii=False, indent=4)
+            # Перезаписываем файл без дублей
+            JSONSaver.write_to_json(self, date_new)
 
-    def delete_aeroplane(self):
-        pass
+    def delete_aeroplane(self,criteria: str):
+        """удвляет данные из файла по позывному"""
+        # Получаем данные из файла
+        data_file = JSONSaver.reaf_data_file(self)
+        date_new = []
+        for item in data_file:
+            if item.get("callsign") != criteria:
+                date_new.append(item)
+        JSONSaver.write_to_json(self, date_new)
