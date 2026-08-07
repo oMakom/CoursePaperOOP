39af4276b8f02a014c2d62ee437dc437f0cfbab8
Your Name <you@example.com>
2026-08-07 20:05:34 +0300

    добавленого кода и интерфейса

--- main.py
+++ main.py
@@ -1,23 +1,17 @@
 from src.aircraft_data_handler import Aeroplane
-from src.api_connector import ApiAeroplanesCounty, ApiCountyCoordinates
-from src.interface import (
-    get_aeroplanes_by_country,
-    filter_aeroplanes,
-    get_aeroplanes_by_altitude,
-    sort_aeroplanes,
-    get_top_aeroplanes,
-    print_all_aeroplanes,
-)
+from src.api_connector import ApiAeroplanesCountry, ApiCountryCoordinates
+from src.interface import (filter_aeroplanes, get_aeroplanes_by_altitude, get_aeroplanes_by_country,
+                           get_top_aeroplanes, print_all_aeroplanes, sort_aeroplanes)
 from src.json_saver import JSONSaver
 
 # Создание экземпляра класса для работы с API страны
-api_county = ApiCountyCoordinates()
+api_county = ApiCountryCoordinates()
 # Получение информации о коордмнатых страны с nominatim.openstreetmap.org
-geo_coord = api_county.get_coordinates("RUSSIA")
+geo_coord = api_county.fetch_data("RUSSIA")
 # Создание экземпляра класса для работы с API самолетов
-api_aeroplanes = ApiAeroplanesCounty()
+api_aeroplanes = ApiAeroplanesCountry()
 # Получение информации о самолетах с opensky-network.org
-aeroplanes_list = api_aeroplanes.get_aeroplanes(geo_coord)
+aeroplanes_list = api_aeroplanes.fetch_data(geo_coord)
 # Преобразование набора данных в список объектов
 aeroplanes = Aeroplane.cast_to_object_list(aeroplanes_list)
 # Пример работы контструктора класса с одним самолетом
@@ -42,24 +36,68 @@ json_saver.delete_aeroplane("DMFEY")
 
 
 def user_interaction():
-    country = input("Введите название страны: ").upper()  # Например RUSSIA
-    aeroplanes = get_aeroplanes_by_country(country)
+    while True:
+        country = input("Введите название страны (пример ввода: RUSSIA): ")  # Например RUSSIA
+        if country:
+            break
+        print("Страна не может быть пустой.")
+    try:
+        aeroplanes = get_aeroplanes_by_country(country)
+    except Exception as e:
+        print(f"Ошибка при запросе данных: {e}")
+        return
+    if not aeroplanes:
+        print("Данные о самолетах не получены. Завершение программы")
+        return
 
     # фильтрация по стране регистрации
     filter_words = (
-        input("Введите названия стран для фильтрации по стране регистрации через запятую: ").title().split(",")
-    )  # например United States, Germany
+        input(
+            "Введите названия стран для фильтрации по стране регистрации через запятую "
+            "(пример ввода: United States, Germany): "
+        )
+        .strip()
+        .split(",")
+    )
+
     filtered_aeroplanes = filter_aeroplanes(aeroplanes, filter_words)
 
     # фильтрация по диапазону высот полета
-    altitude_range = input("Введите диапазон высот полета: ")  # Пример: 200 - 1000
-    ranged_aeroplanes = get_aeroplanes_by_altitude(filtered_aeroplanes, altitude_range)
+    while True:
+        altitude_range = input("Введите диапазон высот полета (Пример ввода: 200 - 10200): ")
+        if altitude_range:
+            range_list = altitude_range.split("-")
+            try:
+                altitude_min = int(range_list[0])
+                altitude_max = int(range_list[1])
+
+                if altitude_min >= altitude_max:
+                    print("Минимальная высота должна быть меньше максимальной")
+                if altitude_min < altitude_max:
+                    ranged_aeroplanes = get_aeroplanes_by_altitude(filtered_aeroplanes, altitude_range)
+                    break
+
+            except ValueError:
+                print("Высота должна быть числом")
+        else:
+            print("Данные диапазона высот не введены. Фильтрация по диапазону высот пропущена")
+            ranged_aeroplanes = filtered_aeroplanes
+            break
 
     # сортировка по высоте полета по убыванию
     sorted_by_altitude = sort_aeroplanes(ranged_aeroplanes)
 
     # вывод топ n самолетов
-    top_n = int(input("Введите количество самолетов для вывода в топ N: "))
+    while True:
+        top_n = input("Введите количество самолетов для вывода в топ N: ").strip()
+        try:
+            n = int(top_n)
+            if n > 0:
+                break
+            print("Число должно быть больше 0.")
+        except ValueError:
+            print("Введите целое число.")
+
     top_n_aeroplanes = get_top_aeroplanes(sorted_by_altitude, top_n)
 
     # вывод результата
